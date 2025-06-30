276a2c28dad51a2bb245ba8b8f3e5196c540a6ad
rugved0102 <rugvedjunghare@gmail.com>
2025-06-30 17:19:54 +0530

    store and tailwind configuration done

---
+++ src/store/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status: false,
+    userData: null,
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: { 
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+    }
+})
+
+export const { login, logout } = authSlice.actions; // reducers me jo actions hai login, logout inko isse taraha export karte hai
+
+export default authSlice.reducer;
