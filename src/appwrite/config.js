024bddb88e8b6ff029fb9a776355e86ee03ba1e0
rugved0102 <rugvedjunghare@gmail.com>
2025-07-04 17:19:12 +0530

    debugging

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,20 +1,20 @@
-import conf from "../conf/conf";
+import conf from '../conf/conf.js';
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
-export class Service {
+export class Service{
     client = new Client();
     databases;
     bucket;
-
-    constructor() {
+    
+    constructor(){
         this.client
-            .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId)
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
         this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
     }
 
-    async createPost({title, slug, content, featuredImage, status, userId}) {
+    async createPost({title, slug, content, featuredImage, status, userId}){
         try {
             return await this.databases.createDocument(
                 conf.appwriteDatabaseId,
@@ -25,16 +25,15 @@ export class Service {
                     content,
                     featuredImage,
                     status,
-                    userId
+                    userId,
                 }
             )
         } catch (error) {
-            console.log("Appwrite Service :: createPost :: error", error);
-            
+            console.log("Appwrite serive :: createPost :: error", error);
         }
     }
 
-    async updatePost(slug, {title, content, featuredImage, status}) {
+    async updatePost(slug, {title, content, featuredImage, status}){
         try {
             return await this.databases.updateDocument(
                 conf.appwriteDatabaseId,
@@ -45,71 +44,74 @@ export class Service {
                     content,
                     featuredImage,
                     status,
+
                 }
             )
         } catch (error) {
-            console.log("Appwrite Service :: updatePost :: error", error)
+            console.log("Appwrite serive :: updatePost :: error", error);
         }
     }
 
-    async deletePost(slug) {
+    async deletePost(slug){
         try {
-          await this.databases.deleteDocument(
-            conf.appwriteDatabaseId,
-            conf.appwriteCollectionId,
-            slug,
-        )  
-        return true
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            
+            )
+            return true
         } catch (error) {
-            console.log("Appwrite Service :: deletePost :: error ", error)
+            console.log("Appwrite serive :: deletePost :: error", error);
             return false
         }
     }
 
-    async getPost(slug) {
+    async getPost(slug){
         try {
             return await this.databases.getDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
-                slug,
+                slug
+            
             )
-
         } catch (error) {
-            console.log("Appwrite Service :: getPost :: error", error);
+            console.log("Appwrite serive :: getPost :: error", error);
             return false
         }
     }
 
-    async getPosts(queries = [Query.equal("status","active")]) {
+    async getPosts(queries = [Query.equal("status", "active")]){
         try {
             return await this.databases.listDocuments(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 queries,
+                
+
             )
         } catch (error) {
-            console.log("Appwrite Service :: getPosts :: error", error);
+            console.log("Appwrite serive :: getPosts :: error", error);
             return false
         }
     }
 
     // file upload service
 
-    async uploadFile(file) {
-        try { // ye method fileId hi return karega
+    async uploadFile(file){
+        try {
             return await this.bucket.createFile(
                 conf.appwriteBucketId,
                 ID.unique(),
-                file,
+                file
             )
         } catch (error) {
-            console.log("Appwrite service :: uploadFile :: error", error);
-            return false;
-            
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
         }
     }
 
-    async deleteFile(fileId) {
+    async deleteFile(fileId){
         try {
             await this.bucket.deleteFile(
                 conf.appwriteBucketId,
@@ -117,19 +119,19 @@ export class Service {
             )
             return true
         } catch (error) {
-            console.log("Appwrite Service :: deleteFile :: error", error);
+            console.log("Appwrite serive :: deleteFile :: error", error);
             return false
         }
     }
 
-    getFilePreview(fileId) {
+    getFilePreview(fileId){
         return this.bucket.getFilePreview(
             conf.appwriteBucketId,
-            fileId,
+            fileId
         )
     }
 }
 
+
 const service = new Service()
 export default service
-// hame Service ka object nikalke chahiye tha jisse direct values mil jayegi
--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import appwriteService from '../appwrite/config'
+import React, {useState, useEffect} from 'react'
 import { Container, PostCard } from '../components'
+import appwriteService from "../appwrite/config";
 
 function AllPosts() {
     const [posts, setPosts] = useState([])
@@ -9,19 +9,18 @@ function AllPosts() {
         if (posts) {
             setPosts(posts.documents)
         }
-    }) // hamne getPosts ke andar kuch bhi pass nhi kiya (empty arr). result me query ek arr return karega
+    })
   return (
     <div className='w-full py-8'>
         <Container>
             <div className='flex flex-wrap'>
                 {posts.map((post) => (
-                    <div key={post.$id} className='p-2 w-1'> 
-                    {/* appwrite hai to $id hai */}
-                        <PostCard post={post}/>
+                    <div key={post.$id} className='p-2 w-1/4'>
+                        <PostCard {...post} />
                     </div>
                 ))}
             </div>
-        </Container>
+            </Container>
     </div>
   )
 }
